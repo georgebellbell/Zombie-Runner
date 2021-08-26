cd607fa4f87aef2ea8201de175d2d90dea6dc434
georgebelll <georgebell.com@gmail.com>
2021-08-26 13:25:10 +0100

    - Created a new scene with a terrain and used probuilder to make a building - Details likes trees, Grass, wall textures and lights have been added for more emersion - Began adding a flashlight to the game that slowly turns off

---
+++ Zombie Runner - FPS/Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = .1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minumumAngle = 40f;
+
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if (myLight.spotAngle <= minumumAngle) { return; }
+        myLight.spotAngle -= angleDecay * Time.deltaTime;
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+}
